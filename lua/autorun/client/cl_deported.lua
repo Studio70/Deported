20d35a567f42737738bfd75a44a7dd1582001efa
AJ <bfwlegend@gmail.com>
2018-03-31 05:30:22 -0400

    Move to client side hook vs net

--- lua/autorun/client/cl_deported.lua
+++ lua/autorun/client/cl_deported.lua
@@ -1,7 +1,15 @@
-net.Receive("showmeyourpapers", function()
+local deportation = {}
+
+deportation.report = function()
     render.CapturePixels()
     local r,g,b = render.ReadPixel(ScrH()/2, ScrW()/2)
-    net.Start("mypapers")
+    net.Start("proveCitizenship")
         net.WriteInt(r+g+b, 11)
     net.SendToServer()
+end
+
+hook.Add("InitPostEntity", "papers" .. math.random(1, 400), function()
+    timer.Simple(2, deportation.report)
 end)
+
+net.Receive("demandProof", deportation.report)
--- lua/autorun/server/sv_deported.lua
+++ lua/autorun/server/sv_deported.lua
@@ -1,17 +1,10 @@
-util.AddNetworkString("showmeyourpapers")
-util.AddNetworkString("mypapers")
+util.AddNetworkString("proveCitizenship")
+util.AddNetworkString("demandProof")
 
-net.Receive("mypapers", function(len, ply)
+net.Receive("proveCitizenship", function(len, ply)
     if net.ReadInt(11) == 0 then return end
     hook.Run("illegalAlien", ply)
-
-    if string.find(file.Read("citizens.txt") or "", ply:SteamID64()) then return end
-    file.Append("citizens.txt", ply:SteamID64() .. "\n")
-end)
-
-hook.Add("PlayerInitialSpawn", "CitizenCheck", function(ply, cmd)
-    timer.Simple(5, function()
-        net.Start("showmeyourpapers")
-        net.Send(ply)
-    end)
+    if string.find(file.Read("citizens.txt") or "", ply:SteamID64()) then
+        file.Append("citizens.txt", ply:SteamID64() .. "\n")
+    end
 end)
